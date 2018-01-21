118fbcaf901227648b05201d54a75d6fb1b0e2a4
Alen <Alenslo@gmx.net>
2018-01-21 12:43:35 +0100

    Active Email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,19 @@
 class StaticPagesController < ApplicationController
 
   def landing_page     #before i called it index
-  @products = Product.limit(4)
+    @products = Product.limit(4)
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'Alenslo@gmx.net',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
+
+
 
 end
