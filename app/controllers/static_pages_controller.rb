3552d2d1c4dd822089c8e595a9979b3077400864
Alen <Alenslo@gmx.net>
2018-02-21 09:16:28 +0100

    deactivated some mail code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,19 +8,11 @@ class StaticPagesController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    ActionMailer::Base.mail(from: @email,
-        to: 'Alenslo@gmx.net',
-        subject: "A new contact form message from #{@name}",
-        body: @message).deliver_now
+    #ActionMailer::Base.mail(from: @email,
+        #to: 'Alenslo@gmx.net',
+        #subject: "A new contact form message from #{@name}",
+        #body: @message).deliver_now
     UserMailer.contact_form(@email, @name, @message).deliver_now
   end
-
-
-  #def thank_you
-    #@name = name
-    #@email = email
-    #@message = message
-    #UserMailer.contact_form(@email, @name, @message).deliver_now
-  #end
 
 end
