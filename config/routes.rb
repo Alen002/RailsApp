66e33a1330506a4267ab987441bde46c2f69ff21
Alen <Alenslo@gmx.net>
2018-02-28 22:43:46 +0100

    Payment functionality created

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   post 'static_pages/thank_you'
 
 
-  get 'payments/create'
+
   post 'payments/create'
 
   root 'static_pages#landing_page'    # root 'static_pages#landing_page'
